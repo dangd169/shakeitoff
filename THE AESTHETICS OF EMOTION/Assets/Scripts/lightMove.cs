4f7e927e3fa7cd43442ccf51b143915901705bfa
Danny <shortieofcheese@gmail.com>
2015-09-29 12:02:23 -0400

    Lights

---
+++ THE AESTHETICS OF EMOTION/Assets/Scripts/lightMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightMove : MonoBehaviour {
+	
+	float targetZ;
+	float targetNZ;
+	public Transform targetRot;
+	public Transform targetNRot;
+	float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float rate = speed * Time.deltaTime;
+
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot.rotation , rate);
+
+
+
+	
+	}
+}
